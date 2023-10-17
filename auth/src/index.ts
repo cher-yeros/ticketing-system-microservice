14b280a511c06f4f6dd20b34809c33cb9e4f83cf
Yerosen Diriba <yerosendiriba1@gmail.com>
2023-10-17 15:36:26 +0300

    mongo service added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,24 @@
 import express from "express";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.use(json());
 
 app.listen(5000, () => {
-  console.log("listening on 5000");
+  start();
+  console.log("listening on http://localhost:5000");
 });
+
+const start = async () => {
+  const url = "mongodb://auth-mongo-srv:27017/auth";
+
+  try {
+    await mongoose.connect(url);
+
+    console.log("Connected to Mongo DB");
+  } catch (error) {
+    console.log(error);
+  }
+};
